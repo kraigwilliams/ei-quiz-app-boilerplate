a1ebb29293b889ccd2b90b223c83f612f6e65a64
KRAIG WILLIAMS <williamskrg@gmail.com>
2019-12-06 17:50:58 -0400

    clean up unused files

--- testing.js
+++
@@ -1,7 +0,0 @@
-
-const answerStrings= STORE.questions[STORE.questionNumber].answers.map(answer => {
-
-    return `<input type="radio" name="question1" value="${answer}">${answer}</input>`
-
-  })
-console.log(answerStrings)
