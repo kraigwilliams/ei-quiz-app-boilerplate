df4281c750772d7c19ed7b8258386801f55aa2f0
KRAIG WILLIAMS <williamskrg@gmail.com>
2019-12-06 17:49:20 -0400

    working code after ta assistance before splitting up functions

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 /**
  * Example store structure
  */
-const store = {
+const STORE = {
   // 5 or more questions are required
   questions: [
     {
+
       question: 'What is the Capital of Florida?',
+      id: 1,
       answers: [
         'Orlando',
         'Miami',
@@ -16,6 +18,7 @@ const store = {
     },
     {
       question: 'What is the Capital of California?',
+      id: 2,
       answers: [
         'Los Angeles',
         'San Diego',
@@ -26,6 +29,7 @@ const store = {
     },
     {
       question: 'What is the Capital of Texas?',
+      id: 3,
       answers: [
         'Austin',
         'Dallas',
@@ -36,6 +40,7 @@ const store = {
     },
     {
       question: 'What is the Capital of Massachusetts?',
+      id: 4,
       answers: [
         'Salem',
         'Boston',
@@ -46,6 +51,7 @@ const store = {
     },
     {
       question: 'What is the Capital of New York?',
+      id: 5,
       answers: [
         'New York',
         'Albany',
@@ -61,30 +67,63 @@ const store = {
   score: 0
 };
 
-let renderQuiz = function(){
+
+
+let renderQuiz = function () {
   /* This function displays html on the screen*/
   console.log('render function ran');
-  $('body').html(`<header><h1>State Quiz App</h1> </header> <main>
+  const startQuizPage = `<header><h1>State Quiz App</h1> </header> <main>
   <h3>Do you know your state capitals?</h3>
   <h4>Let's find out!</h4>
-  <form action= "https://thinkful-ei-jaguar.github.io/Quiz_AppCK/question1_wireframe.html">
-      <input type= "submit" value= "Submit Answer">
-          </form>
+ 
+  <button id="startQuizbtn">Start Quiz</button>
       
   <br>
   <br>
   <img src="#" alt="Map of the United States"> 
-  </main>`)
+  </main>`
+
+  $('body').html(startQuizPage)
+
 };
 
-let startQuiz =function(){
+let startQuiz = function () {
   /*This function allow the user to press a button
   to start the quiz and display the first question */
+
+
   console.log('startQuiz function ran');
 
+  $('body').on("click", "#startQuizbtn", function (event) {
+    const finalStrings =[];
+    let questHeaders = `<h2>${STORE.questions[STORE.questionNumber].question} </h2>`;
+    finalStrings.push(questHeaders);
+
+
+    const answerStrings = STORE.questions[STORE.questionNumber].answers.map(answer => {
+
+      return `<input type="radio" name="question1" value="${answer}">${answer}</input><br><br>`
+
+    })
+
+
+let finalAnswers =answerStrings.join("")
+finalStrings.push(finalAnswers);
+const submitAnswetBtn = `<input type ="submit" value="Final Answer"</input>`;
+finalStrings.push(submitAnswetBtn)
+$('body').html(finalStrings.join(""))
+  })
 }
 
-let nextQuestion = function(){
+function showQuestion(question) {
+  console.log("pulling questions from store");
+
+  const questions = questions.map((item) => generateItemElement(item));
+
+  return items.join("");
+}
+
+let nextQuestion = function () {
   /*This function allows the user to press a button
   after they have answered the current question and 
   received feedback to then move on the the next question 
@@ -93,22 +132,34 @@ let nextQuestion = function(){
   console.log('nextQuestion function ran');
 }
 
-let checkAnswer = function(){
-  /*This function runs when the user submits thier answer
-  to the current question and checks their response with
-  the correct answer then display whether they were right or wrong. */
-  console.log('checkAnswer function ran');
+//let checkAnswer = function(){
+/*This function runs when the user submits thier answer
+to the current question and checks their response with
+the correct answer then display whether they were right or wrong. */
+/*console.log('checkAnswer function ran');
+let currentQuestion= $('form').val();
+console.log(currentQuestion)
+*/
+/*$('form').on("submit", ".question1" ,function(event){
+  event.preventDefault();
+  if event.id = RTC
+})
+*/
 
+/*store.forEach(function(ques){
+if (FormData. ==)
+})*/
+
+
+
+let restartQuiz = function () {
+  /* This function runs when the user is finishes the quiz and can 
+  submit using a button to do the quiz over from scratch  beginning
+  with quesition one*/
+  console.log('restartQuiz function ran');
 }
 
-let restartQuiz = function(){
-/* This function runs when the user is finishes the quiz and can 
-submit using a button to do the quiz over from scratch  beginning
-with quesition one*/
-console.log('restartQuiz function ran');
-}
-
-let showResults= function(){
+let showResults = function () {
   /* This functions is run when the user submits the final answer
   to the quiz questions and then displays the summary of the
   quiz showing their total score on the quiz */
@@ -116,8 +167,10 @@ let showResults= function(){
   console.log('showResults function ran');
 }
 
-let handleQuizApp = function(){
-  renderQuiz()
+let handleQuizApp = function () {
+  renderQuiz();
+  startQuiz();
+
 }
 
 // when the page loads, call `handleQuizApp`
